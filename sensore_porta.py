2383d16529b7c08d131aeef16fcf28a4a8c55bd3
Gianluca Romito <gianluca.romito@gmail.com>
2017-08-24 18:16:16 +0200

    Aggiunto invo mails

--- sensore_porta.py
+++ sensore_porta.py
@@ -5,7 +5,7 @@ import logging
 import logging.handlers
 
 #logging, max 2M a file e ne tengo solo 5
-LOG_FILENAME = 'sensore_porte.log'
+LOG_FILENAME = '/home/pi/python/sensore_porta/sensore_porta.log'
 logger = logging.getLogger('sensore_porta')
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s (%(threadName)-10s) %(levelname)s %(message)s')
@@ -17,6 +17,30 @@ consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(formatter)
 logger.addHandler(consoleHandler)
 
+def mail(sub):
+	import smtplib
+	from email.MIMEMultipart import MIMEMultipart
+	from email.MIMEText import MIMEText
+ 
+ 
+	fromaddr = "gianluca.romito@gmail.com"
+	toaddr ="gianluca.romito@gmail.com"
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = sub
+ 
+	body = ""
+	msg.attach(MIMEText(body, 'plain'))
+ 
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login(fromaddr, "ghcgnwkqmgpfageb")
+	text = msg.as_string()
+	server.sendmail(fromaddr, toaddr, text)
+	server.quit()
+
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -24,10 +48,12 @@ stato_prec = True #True se chiuso False se aperto
 while True:
 	input_state = GPIO.input(18)
 	if input_state == True and stato_prec == False :
-		logger.info('Porta aperta')
+		mail('Porta via Sacchi aperta')
+		logger.info('Porta via Sacchi aperta')
 		stato_prec = True
 	elif input_state == False and stato_prec == True:
-		logger.info('Porta chiusa')
+		mail('Porta via Sacchi chiusa')
+		logger.info('Porta Via Sacchi chiusa')
 		stato_prec = False
 	
 	time.sleep(0.2)
